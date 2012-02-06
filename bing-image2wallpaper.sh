1b55ced76a0c2543fd636892e49b88ccc616a75c
Leonardo Weslei Diniz <leonardoweslei@gmail.com>
2012-02-06 11:00:53 -0200

    Adicionando suporte ao LXDE/LUbuntu Melhorando desenpenho Adicionando configuração de marca dagua e diretorio para salvar imagens

--- bing-image2wallpaper.sh
+++ bing-image2wallpaper.sh
@@ -18,6 +18,9 @@ function background()
 		if [ "$DESKTOP_SESSION" == "ubuntu-2d" ] || [ "$DESKTOP_SESSION" == "ubuntu" ] || [ "$DESKTOP_SESSION" == "gnome-shell" ] || [ "$DESKTOP_SESSION" == "gnome-classic" ] || [ "$DESKTOP_SESSION" == "gnome-fallback" ] || [ "$DESKTOP_SESSION" == "gnome-classic" ] ; then
 			gsettings set org.gnome.desktop.background picture-uri "file://"$2;
 			return 1;
+		elif [ "$DESKTOP_SESSION" == "kde-plasma" ]; then
+			kwriteconfig --file plasma-desktop-appletsrc --group Containments --group 1 --group Wallpaper --group image --key wallpaper $2;
+			return 1;
 		elif [ "$DESKTOP_SESSION" == "xfce" ] || [ "$DESKTOP_SESSION" == "xubuntu" ] ; then
 			xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s $2;
 			return 1;
@@ -27,16 +30,43 @@ function background()
 		elif [ "$DESKTOP_SESSION" == "fluxbox" ]; then
 			fbsetbg -f $2;
 			return 1;
+		elif [ "$DESKTOP_SESSION" == "Lubuntu" ] || [ "$DESKTOP_SESSION" == "LXDE" ]; then
+			pcmanfm --wallpaper-mode=stretch --set-wallpaper=$2;
+			return 1;
+		elif [ "$(which gconftool-2)" != "" ] ; then
+			gconftool-2 --type string --set /desktop/gnome/background/picture_filename $2;
+			return 1;
+		elif [ "$(which gconftool)" != "" ] ; then
+			gconftool --type string --set /desktop/gnome/background/picture_filename $2;
+			return 1;
 		fi
 	elif [ "$1" == "get" ] ; then
 		if [ "$DESKTOP_SESSION" == "ubuntu-2d" ] || [ "$DESKTOP_SESSION" == "ubuntu" ] || [ "$DESKTOP_SESSION" == "gnome-shell" ] || [ "$DESKTOP_SESSION" == "gnome-classic" ] || [ "$DESKTOP_SESSION" == "gnome-fallback" ] || [ "$DESKTOP_SESSION" == "gnome-classic" ] ; then
 			echo $(echo $(gsettings get org.gnome.desktop.background picture-uri)| sed 's/file:\/\///g'| sed "s/'//g");
+			return 1;
+		elif [ "$DESKTOP_SESSION" == "kde-plasma" ]; then
+			echo $(kreadconfig --file plasma-desktop-appletsrc --group Containments --group 1 --group Wallpaper --group image --key wallpaper);
 			return 1;
 		elif [ "$DESKTOP_SESSION" == "xfce" ] || [ "$DESKTOP_SESSION" == "xubuntu" ] ; then
 			echo $(xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path);
 			return 1;
 		elif [ "$DESKTOP_SESSION" == "mate" ]; then
 			echo $(mateconftool-2 --get /desktop/mate/background/picture_filename);
+			return 1;
+		elif [ "$DESKTOP_SESSION" == "fluxbox" ]; then
+			echo $(cat ~/.fluxbox/lastwallpaper | grep full | cut -d '|' -f2);
+			return 1;
+		elif [ "$DESKTOP_SESSION" == "Lubuntu" ]; then
+			echo $(cat ~/.config/pcmanfm/lubuntu/pcmanfm.conf | egrep 'wallpaper=(.*)'| sed 's/wallpaper=//g');
+			return 1;
+		elif [ "$DESKTOP_SESSION" == "LXDE" ]; then
+			echo $(cat ~/.config/pcmanfm/LXDE/pcmanfm.conf | egrep 'wallpaper=(.*)'| sed 's/wallpaper=//g');
+			return 1;
+		elif [ "$(which gconftool-2)" != "" ] ; then
+			echo $(gconftool-2 --get /desktop/gnome/background/picture_filename);
+			return 1;
+		elif [ "$(which gconftool)" != "" ] ; then
+			echo $(gconftool --get /desktop/gnome/background/picture_filename);
 			return 1;
 		fi
 	elif [ "$1" == "remove" ] ; then
@@ -52,6 +82,15 @@ function background()
 		elif [ "$DESKTOP_SESSION" == "fluxbox" ]; then
 			/usr/bin/fbsetroot -solid black;
 			return 1;
+		elif [ "$DESKTOP_SESSION" == "Lubuntu" ] || [ "$DESKTOP_SESSION" == "LXDE" ]; then
+			pcmanfm --wallpaper-mode=color;
+			return 1;
+		elif [ "$(which gconftool-2)" != "" ] ; then
+			gconftool-2 --unset /desktop/gnome/background/picture_filename;
+			return 1;
+		elif [ "$(which gconftool)" != "" ] ; then
+			gconftool --unset /desktop/gnome/background/picture_filename;
+			return 1;
 		fi
 	else
 		echo -e "Use\n\tbackground get\n ou \n\tbackground remove\n ou \n\tbackground set <url>";
@@ -59,31 +98,25 @@ function background()
 	return 0;
 }
 mk=~/Dropbox/projetos/marcadagua.jpg;
+dir=~/bing_images;
 echo -e "Iniciando sistema...\n";
 data=$(date +"%Y%m%d");
 atoi $(date +"%H");
 hora=$?;
-paises[0]="us";
-#paises[1]="us";
-#paises[2]="uk";
-#paises[3]="au";
-#paises[4]="jp";
-#paises[5]="cn";
-#paises[6]="nz";
-#paises[7]="de";
-#paises[8]="ca";
-pais=$(($RANDOM%$hora));
-pais=$(($pais%${#paises[*]}));
-pais=${paises[$pais]};
-urld=~/bing_images/$data-$pais.jpg;
+if [ ! -e $dir ]; then
+	mkdir $dir;
+fi
+urld=$dir/$data.jpg;
+urld2=$dir/$data"2.jpg";
 while :; do
 	datanow=$(date +"%Y%m%d");
 	atoi $(date +"%H");
 	horanow=$?;
-	if [ -e $urld ] || [ $(du -sh $urld | cut -f1) -eq 0 ] || [ $(file $urld | cut -d ' ' -f3) != "image" ] ; then
+	bgactive=$(background "get");
+	if [ -e $urld ] || [ "$(du -sh $urld | cut -f1)" == "" ] || [ "$(file $urld | cut -d ' ' -f3)" != "image" ]; then
 		tam=$(du -sh $urld | cut  -f1);
 		tam=${tam%%[!0-9]*};
-		if [ "$tam" -eq 0 ] ; then
+		if [ "$tam" == "" ]; then
 			echo -e "Apagando imagem vazia...\n";
 			rm -rf $urld;
 			sleep 0;
@@ -95,22 +128,13 @@ while :; do
 		data=$(date +"%Y%m%d");
 		atoi $(date +"%H");
 		hora=$?;
-		pais=$(($RANDOM%$hora));
-		pais=$(($pais%${#paises[*]}));
-		pais=${paises[$pais]};
-		urld=~/bing_images/$data-$pais.jpg;
-		urld2=~/bing_images/$data-$pais"2.jpg";
-		if [ ! -e "$HOME/bing_images/" ] ; then
-			echo -e "Criando diretorio de imagens...\n";
-			mkdir ~/bing_images;
-		fi
+		urld=$dir/$data.jpg;
+		urld2=$dir/$data"2.jpg";
 		if [ ! -e $urld ] ; then
-			cd /tmp;
-			rm .tmp.txt*;
-			echo -e "Baixando imagem$data-${paises[$hora]}.jpg do bing...\n";
-			wget $pais.bing.com -O .tmp.txt >/dev/null;
-			img=$(cat .tmp.txt | sed "s/[;,:']/\n/g" | egrep "(.*)az(.*)\.(png|jpg|jpeg)$");
-			#$(cat .tmp.txt | sed "s/[;,']/\n/g" | egrep "url(.*)(png|jpg|jpeg)$" | sed 's/\\//g');
+			temp=$(tempfile);
+			echo -e "Baixando imagem do bing...\n";
+			wget bing.com -O $temp >/dev/null;
+			img=$(cat $temp | sed "s/[;,:']/\n/g" | egrep "(.*)az(.*)\.(png|jpg|jpeg)$");
 			url="www.bing.com$img";
 			wget $url -O $urld >/dev/null;
 			cd $OLDPWD;
@@ -125,7 +149,12 @@ while :; do
 		background "remove";
 		echo -e "Setando background...\n";
 		background "set" $urld;
-		sleep $((60*15));
-		#sleep 1;
+		sleep 10;
+	elif [ $bgactive != $urld2 ]  && [ -e $urld2 ] ; then
+		echo -e "Setando background...\n";
+		background "set" $urld2;
+		sleep 10;
+	else
+		sleep $((60*60));
 	fi
 done
